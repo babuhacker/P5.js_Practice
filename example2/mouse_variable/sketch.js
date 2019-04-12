c769d94cfbf0d0decb45b5cf8f38c2c3716da400
Vikas <42447141+babuhacker@users.noreply.github.com>
2019-04-12 11:13:42 -0500

    Add files via upload

---
+++ example2/mouse_variable/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+   background('#1d1919');
+}
+
+function draw() {
+   
+ 
+
+ fill('#ffce76');
+  noStroke();
+ ellipse(mouseX,mouseY,20,20);
+ 
+  
+}
+function mousePressed(){
+     background('#1d1919');
+    
+}
